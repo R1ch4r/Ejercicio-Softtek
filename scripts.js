b01ce2935d29adfa71b847e1c3482d3e8c4589ea
R1ch4r <ricardo@rrestudio.com>
2014-10-06 18:24:19 -0500

    Update scripts.js
    Inicialización del slider

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-$(document).ready(function(e) {
-    console.log("Hello World");
+$(document).ready(function() {
+    $('.slides').slick({
+		dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		autoplay: true
+	});
 });
