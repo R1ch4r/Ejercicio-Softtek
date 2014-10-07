1e889efb7a3c8a73d8f24604e0d9431ccc7674eb
R1ch4r <ricardo@rrestudio.com>
2014-10-07 15:17:16 -0500

    Update scripts.js
    ajustes a la inicialización del slideshow

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     $('.slides').slick({
-		dots: false,
+	dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-		autoplay: true
+	autoplay: true,
+	adaptiveHeight: true,
 	});
 });
